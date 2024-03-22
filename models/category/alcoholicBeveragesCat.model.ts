22f620624aeba42ca72c9c9bdd2d2c0b29eec564
Avanin <asamuvictor03@gmail.com>
2024-03-22 22:05:51 +0100

    updated

--- models/category/alcoholicBeveragesCat.model.ts
+++ models/category/alcoholicBeveragesCat.model.ts
@@ -5,7 +5,7 @@ const alcoholicBeveragesCatSchema = new Schema({
     productName:{type:String,required:true},
     productCategory:{type:String,required:true},
     productdescription:{type:String,required:true},
-    productPrice:{type:String,required:true}
+    productPrice:{type:String,required:true},
 });
 
 export const alcoholicBeveragesCatModel = mongoose.model("alcohBeverCat_collections", alcoholicBeveragesCatSchema);
