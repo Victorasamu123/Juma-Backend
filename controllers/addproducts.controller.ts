855dc02057acceafaf8580df900a9ec1842f641f
Avanin <asamuvictor03@gmail.com>
2024-03-25 12:10:56 +0100

    update myProductUploader Contollers

--- controllers/addproducts.controller.ts
+++ controllers/addproducts.controller.ts
@@ -21,18 +21,120 @@ cloudinaryV2.config({
 
 export const myProductUploader = async (req:Request,res:Response,next:NextFunction) => {
     console.log(req.body);
-    const myProductFile = req.body.productImage
-
-    try {
-        let uploaded = await cloudinaryV2.uploader.upload(myProductFile);
-        if(uploaded){
-            console.log(uploaded.secure_url);
-            const myImage = uploaded.secure_url
-            let newProduct = new productCatModel({...req.body,productImage:myImage});
+    if(req.body.productCategory === "snacks"){
+        const myProductFile = req.body.productImage
+        try {
+            let uploaded = await cloudinaryV2.uploader.upload(myProductFile);
+            if(uploaded){
+                console.log(uploaded.secure_url);
+                const myImage = uploaded.secure_url
+                let newProduct = new productCatModel({...req.body,productImage:myImage});
+                if(newProduct){
+                    res.send({message:"product upload was successful"});
+                }
+            }
+        } catch (error) {
+            console.log("File did not upload")
+            res.send({message:"upload failed",status:false})
         }
-        // let savvedProduct = 
-    } catch (error) {
-        console.log("File did not upload")
-        res.send({message:"upload failed",status:false})
+    }else if(req.body.productCategory === "saucesAndCondiments"){
+        const myProductFile = req.body.productImage
+        try {
+            let uploaded = await cloudinaryV2.uploader.upload(myProductFile);
+            if(uploaded){
+                console.log(uploaded.secure_url);
+                const myImage = uploaded.secure_url
+                let newProduct = new saucesCondimentCatModel({...req.body,productImage:myImage});
+                if(newProduct){
+                    res.send({message:"product upload was successful"});
+                }
+            }
+        } catch (error) {
+            console.log("File did not upload")
+            res.send({message:"upload failed",status:false});
+        }
+    }else if(req.body.productCategory === "ethnicFood"){
+        const myProductFile = req.body.productImage
+        try {
+            let uploaded = await cloudinaryV2.uploader.upload(myProductFile);
+            if(uploaded){
+                console.log(uploaded.secure_url);
+                const myImage = uploaded.secure_url
+                let newProduct = new ethnicFoodCatModel({...req.body,productImage:myImage});
+                if(newProduct){
+                    res.send({message:"product upload was successful"});
+                }
+            }
+        } catch (error) {
+            console.log("File did not upload")
+            res.send({message:"upload failed",status:false});
+        }
+    }else if(req.body.productCategory === "bakeryItems"){
+        const myProductFile = req.body.productImage
+        try {
+            let uploaded = await cloudinaryV2.uploader.upload(myProductFile);
+            if(uploaded){
+                console.log(uploaded.secure_url);
+                const myImage = uploaded.secure_url
+                let newProduct = new bakeryItemsCatModel({...req.body,productImage:myImage});
+                if(newProduct){
+                    res.send({message:"product upload was successful"});
+                }
+            }
+        } catch (error) {
+            console.log("File did not upload")
+            res.send({message:"upload failed",status:false});
+        }
+    }else if(req.body.productCategory === "softDrinks"){
+        const myProductFile = req.body.productImage
+        try {
+            let uploaded = await cloudinaryV2.uploader.upload(myProductFile);
+            if(uploaded){
+                console.log(uploaded.secure_url);
+                const myImage = uploaded.secure_url
+                let newProduct = new softDrinksCatModel({...req.body,productImage:myImage});
+                if(newProduct){
+                    res.send({message:"product upload was successful"});
+                }
+            }
+        } catch (error) {
+            console.log("File did not upload")
+            res.send({message:"upload failed",status:false});
+        }
+    }else if(req.body.productCategory === "alcoholicBeverages"){
+        const myProductFile = req.body.productImage
+        try {
+            let uploaded = await cloudinaryV2.uploader.upload(myProductFile);
+            if(uploaded){
+                console.log(uploaded.secure_url);
+                const myImage = uploaded.secure_url
+                let newProduct = new alcoholicBeveragesCatModel({...req.body,productImage:myImage});
+                if(newProduct){
+                    res.send({message:"product upload was successful"});
+                }
+            }
+        } catch (error) {
+            console.log("File did not upload")
+            res.send({message:"upload failed",status:false});
+        }
+    }else if(req.body.productCategory === "desertAndSweets"){
+        const myProductFile = req.body.productImage
+        try {
+            let uploaded = await cloudinaryV2.uploader.upload(myProductFile);
+            if(uploaded){
+                console.log(uploaded.secure_url);
+                const myImage = uploaded.secure_url
+                let newProduct = new desertSweetsCatModel({...req.body,productImage:myImage});
+                if(newProduct){
+                    res.send({message:"product upload was successful"});
+                }
+            }
+        } catch (error) {
+            console.log("File did not upload")
+            res.send({message:"upload failed",status:false});
+        }
+    }else{
+        res.send({message:"invalid category"});
     }
+    
 };
