8edf0c7db9c83d40aeee89588731b89b979679c0
Avanin <asamuvictor03@gmail.com>
2024-04-09 22:51:19 +0100

    created delete func

--- controllers/productControl.controller.ts
+++ controllers/productControl.controller.ts
@@ -1,11 +1,15 @@
 import { Express, Request, Response, NextFunction } from "express";
 import { snacksCatModel } from "../models/category/snacksCat.model";
 
-export const deleteProduct = (req:Request,res:Response, next:NextFunction) =>{
+export const deleteProduct = async (req:Request,res:Response, next:NextFunction) =>{
     console.log(req.body);
     try {
-        let deletedProduct = snacksCatModel.findByIdAndDelete({id:req.body.productId});
+        let deletedProduct = await snacksCatModel.findByIdAndDelete({id:req.body.productId});
+        if(deletedProduct){
             res.send({message:"Product has been deleted successfully", status:true});
+        }else{
+            res.send({message:"Product has been deleted was not successfully", status:false});
+        }
     } catch (error) {
         if(error){
             res.send({message:"An error occured", status:false});
--- dist/controllers/productControl.controller.js
+++ dist/controllers/productControl.controller.js
@@ -1,17 +1,31 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteProduct = void 0;
 const snacksCat_model_1 = require("../models/category/snacksCat.model");
-const deleteProduct = (req, res, next) => {
+const deleteProduct = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     console.log(req.body);
     try {
-        let deletedProduct = snacksCat_model_1.snacksCatModel.findByIdAndDelete({ id: req.body.productId });
-        res.send({ message: "Product has been deleted successfully", status: true });
+        let deletedProduct = yield snacksCat_model_1.snacksCatModel.findByIdAndDelete({ id: req.body.productId });
+        if (deletedProduct) {
+            res.send({ message: "Product has been deleted successfully", status: true });
+        }
+        else {
+            res.send({ message: "Product has been deleted was not successfully", status: false });
+        }
     }
     catch (error) {
         if (error) {
             res.send({ message: "An error occured", status: false });
         }
     }
-};
+});
 exports.deleteProduct = deleteProduct;
